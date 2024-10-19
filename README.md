e0af6628b21627b54be4c9cf76ae01226ae77aa5
Dzidoula Godwin KPOGLO <kpoglo.godwin10@gmail.com>
2024-10-19 13:03:35 +0200

    add unique constrain to property reference in table Order and start translations

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # symfony_project
 Symfony project for academic year 2024-2025 at IUT de Metz
 
-#Sujet du projet:
+# Sujet du projet:
 Décoration intérieure
--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -18,7 +18,7 @@ class Order
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255,unique: true)]
     private ?string $reference = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
