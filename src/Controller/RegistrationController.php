f0e264b83b6ab349bcee5c26f2fa15ccbf7450fd
Dzidoula Godwin KPOGLO <kpoglo.godwin10@gmail.com>
2024-10-20 16:15:59 +0200

    registration done

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var string $plainPassword */
+            $plainPassword = $form->get('plainPassword')->getData();
+            //Par défaut, le rôle user à l'utilisateur
+            $role = ['ROLE_USER'];
+
+            // Si le champ 'role' est présent et que l'utilisateur connecté est un administrateur
+            if ($this->isGranted('ROLE_ADMIN') && $form->has('role')) {
+                $role = [$form->get('role')->getData()];
+            }
+            $user->setRoles($role);
+
+            // encode the plain password
+            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form,
+        ]);
+    }
+}
